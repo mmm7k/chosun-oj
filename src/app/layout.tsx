81ce0e5554b081b0ef2f8ccdfc19071a9b471c36
mmm7k <ya5656@naver.com>
2024-11-14 15:45:34 +0900

    feat: add robots metatag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
         />
+        <meta name="robots" content="noindex, nofollow" />
       </head>
       <body className={notoSansKr.className}>{children}</body>
     </html>
