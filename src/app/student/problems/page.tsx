9c6ee2dae005b57ee6dd146a3dcaacd0f009bb2d
mmm7k <ya5656@naver.com>
2024-11-22 16:36:56 +0900

    fix: suspense error

--- src/app/student/problems/page.tsx
+++ src/app/student/problems/page.tsx
@@ -1,5 +1,6 @@
 import ProblemList from '@/components/student/problems/ProblemList';
 import ProblemsBanner from '@/components/student/problems/ProblemsBanner';
+import { Suspense } from 'react';
 
 export default function Problems() {
   return (
@@ -7,7 +8,9 @@ export default function Problems() {
       <ProblemsBanner />
       <main className="bg-[#f0f4fc] min-h-screen w-full flex  justify-center">
         <div className="w-[90%] lg:w-[62%] pt-12 mb-44">
-          <ProblemList />
+          <Suspense>
+            <ProblemList />
+          </Suspense>
         </div>
       </main>
     </>
